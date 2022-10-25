5e5194a6cf454262824b4f4f481e550f7d4fe0db
Crash2022 <crash555@mail.ru>
2022-10-25 21:55:24 +0500

    add redux dev tools util

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,6 +2,13 @@ import {combineReducers, legacy_createStore} from "redux";
 import {tasksReducer} from "./tasks-reducer";
 import {todolistsReducer} from "./todolists-reducer";
 
+// для React Redux Tools Chrome
+// declare global {
+//     interface Window {
+//         __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+//     }
+// }
+
 const rootReducer = combineReducers({
     todolists: todolistsReducer,
     tasks: tasksReducer
@@ -11,6 +18,11 @@ export const store = legacy_createStore(rootReducer);
 //export const store = createStore(rootReducer); // старая версия записи
 
 export type AppRootStateType = ReturnType<typeof rootReducer>;
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// // непосредственно создаём store
+// export const store = createStore(rootReducer, composeEnhancers());
+
 
 // @ts-ignore
 window.store = store;
