678c18f6596cab7e2deb81b9ccf6c87413090fce
Crash2022 <crash555@mail.ru>
2022-10-25 13:55:20 +0500

    add storybook

---
+++ src/stories/AddItemForm.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {AddItemForm} from "../UI/AddItemForm";
+import {ComponentMeta} from "@storybook/react";
+import {action} from "@storybook/addon-actions";
+
+export default {
+    title: 'AddItemForm StoryComponent',
+    component: AddItemForm
+} as ComponentMeta<typeof AddItemForm>;
+
+const callback = action('Button ADD was pressed inside AddItemForm');
+
+export const TaskBaseExample = (props: any) => {
+    return (
+        <AddItemForm addItem={callback}/>
+    );
+}
---
+++ src/stories/EditableSpan.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {EditableSpan} from "../UI/EditableSpan";
+import {ComponentMeta} from "@storybook/react";
+import {action} from "@storybook/addon-actions";
+
+export default {
+    title: 'EditableSpan StoryComponent',
+    component: EditableSpan
+} as ComponentMeta<typeof EditableSpan>;
+
+const callback = action('Title was changed inside EditableSpan');
+
+export const EditableSpanBaseExample = (props: any) => {
+    return (
+        <EditableSpan title={'Click to change this title'} onChangeInput={callback}/>
+    );
+}
