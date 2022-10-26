1119b0ecb172815a73a206b24a067cf384986e90
Crash2022 <crash555@mail.ru>
2022-10-26 11:37:01 +0500

    config storybook tests

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,27 @@ module.exports = {
         "@storybook/addon-links",
         "@storybook/addon-essentials",
         "@storybook/addon-interactions",
-        "@storybook/preset-create-react-app"
+        "@storybook/preset-create-react-app",
+        {
+            name: '@storybook/addon-storysource',
+            options: {
+                loaderOptions: {
+                    injectStoryParameters: false,
+                },
+            },
+        },
+        // {
+        //     name: '@storybook/addon-storysource',
+        //     options: {
+        //         rule: {
+        //             // test: [/\.stories\.jsx?$/], This is default
+        //              include: [path.resolve(__dirname, '../src')], // You can specify directories
+        //         },
+        //         loaderOptions: {
+        //             prettierConfig: { printWidth: 80, singleQuote: false },
+        //         },
+        //     },
+        // },
     ],
     "framework": "@storybook/react",
     "core": {
--- integration/addItemForm.test.js
+++ integration/addItemForm.test.js
@@ -1,3 +1,7 @@
+// storybook.test.js
+// import initStoryshots from '@storybook/addon-storyshots';
+// initStoryshots();
+
 describe('addItemForm', () => {
     it('base example looks correct', async () => {
         await page.goto('http://localhost:6006/iframe.html?id=additemform-storycomponent--task-base-example&viewMode=story');
