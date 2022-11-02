0f2c6ded46407c5bf645d6efb79f2c9b9fe5c9dc
Crash2022 <crash555@mail.ru>
2022-11-02 16:27:53 +0500

    storybook tests corrected

--- integration/addItemForm.test.js
+++ integration/addItemForm.test.js
@@ -4,9 +4,11 @@
 
 describe('addItemForm', () => {
     it('base example looks correct', async () => {
-        await page.goto('http://localhost:6006/iframe.html?id=additemform-storycomponent--task-base-example&viewMode=story');
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=additemform-storycomponent--task-base-example&viewMode=story');
         const image = await page.screenshot();
 
+        // API from jest-image-snapshot
         expect(image).toMatchImageSnapshot();
     });
 });
--- integration/jest.config.js
+++ integration/jest.config.js
@@ -3,6 +3,3 @@ module.exports = {
     testRegex: '.*/\\.test\\.js$',
     setupFilesAfterEnv: ['./setupTests.js']
 }
-
-//"jest:integration": "jest -c integration/jest.config.js",
-//"test:integration": "start-server-and-test storybook http-get://localhost:6006 jest:integration"
