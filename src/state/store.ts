7d710a3c63a0e1dcef9e8c43d1ff9f6078b63178
Crash2022 <crash555@mail.ru>
2023-01-10 11:06:02 +0500

    refactor: store

--- src/state/store.ts
+++ src/state/store.ts
@@ -7,6 +7,8 @@ import {loginReducer} from './login-reducer';
 import {configureStore} from '@reduxjs/toolkit';
 import {rootReducer} from './reducers'
 
+/*------------------------------------------------------------*/
+
 // для React Redux DevTools Chrome
 declare global {
     interface Window {
@@ -19,7 +21,44 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // добавляем composeEnhancers() для React Redux DevTools Chrome
 // export const store = createStore(rootReducer, composeEnhancers());
 
+/*------------------------------------------------------------*/
 
+// hot reloading-replacement
+if (process.env.NODE_ENV === 'development' && module.hot) {
+    module.hot.accept('./reducers', () => {
+        store.replaceReducer(rootReducer)
+    })
+}
+
+/*------------------------------------------------------------*/
+// REDUX TOOLKIT
+
+// rootReducer без Hot Reloading
+// const rootReducer = combineReducers({
+//     todolists: todolistsReducer,
+//     tasks: tasksReducer,
+//     app: appReducer,
+//     auth: loginReducer
+// })
+
+// redux-toolkit store
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunkMiddleware)
+})
+
+// типизация state
+export type AppRootStateType = ReturnType<typeof rootReducer>;
+// export type AppRootStateType = ReturnType<typeof store.getState> // типизация из документации
+
+export type AppDispatch = typeof store.dispatch;
+
+export type AppThunkType<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, any>
+
+/*------------------------------------------------------------*/
+// REACT REDUX
+
+// rootReducer без Hot Reloading
 // const rootReducer = combineReducers({
 //     todolists: todolistsReducer,
 //     tasks: tasksReducer,
@@ -31,32 +70,10 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // @ts-ignore // для Chrome Extension
 // export const store = legacy_createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
-// redux-toolkit store
-export const store = configureStore({
-    reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunkMiddleware)
-})
-
-// hot reloading-replacement
-if (process.env.NODE_ENV === 'development' && module.hot) {
-    module.hot.accept('./reducers', () => {
-        store.replaceReducer(rootReducer)
-    })
-}
-
-// типизация state
-export type AppRootStateType = ReturnType<typeof rootReducer>;
-// export type AppRootStateType = ReturnType<typeof store.getState> // типизация из документации
-
-// типизация Dispatch и Selector (useHooks)
-// export const useAppDispatch = () => useDispatch<AppDispatch>();
-// export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector;
-
-// react-redux
 // export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, any>;
-// redux-toolkit
-export type AppDispatch = typeof store.dispatch;
 
+// типизация Thunk React-Redux
+// export type AppThunkType<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AppActionType>
 
 // типизация всех экшенов для React-Redux
 // export type AppActionType =
@@ -65,11 +82,7 @@ export type AppDispatch = typeof store.dispatch;
 //     ApplicationActionTypes |
 //     LoginActionTypes;
 
-// типизация Thunk
-// react-redux
-// export type AppThunkType<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AppActionType>
-// redux-toolkit
-export type AppThunkType<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, any>
+/*------------------------------------------------------------*/
 
 // @ts-ignore
 window.store = store;
